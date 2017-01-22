a38ef3eb920a34051ee9e66f916f6def0f177289
Colin Lyons <colin@tercet.io>
2017-01-22 22:47:54 +0000

    Changing Result contract.

--- src/server/vm/query/result.go
+++ src/server/vm/query/result.go
@@ -1,14 +1,6 @@
 package query
 
-import (
-	"github.com/domain-query-language/dql-server/src/server/vm"
-)
-
 type Result interface {
-
-	Id() vm.Identifier
-
-	TypeId() vm.Identifier
 
 	MarshalJSON() ([]byte, error)
 }
