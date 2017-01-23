410c91d74da3f07b0e1c4924ccf407a0af5c46b5
Barry O Sullivan <barry@tercet.io>
2017-01-23 22:22:27 +0000

    Split the adapter into two, one for queries and one for commands. Use the interfaces previously defined.

--- src/server/adapter/adapter.go
+++ src/server/adapter/adapter.go
@@ -1,17 +1,15 @@
 package adapter
 
-type Adapter interface {
-	Next() (Handleable, error)
-}
-
-type Handleable interface {
-	Type() (HandleableType)
-}
-
-type HandleableType string
-
-const (
-	CMD HandleableType = "command"
-	QRY = "query"
+import (
+	"github.com/domain-query-language/dql-server/src/server/vm/handler/command"
+	"github.com/domain-query-language/dql-server/src/server/vm/handler/query"
 )
+
+type CommandAdapter interface {
+	Next() (command.Command, error)
+}
+
+type QueryAdapter interface {
+	Next() (query.Query, error)
+}
 
