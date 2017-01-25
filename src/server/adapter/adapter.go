c56620cf2f25c751aec3bb4e53d5585b01528116
Barry O Sullivan <barry@tercet.io>
2017-01-25 12:25:59 +0000

    Refactor to just use a struct instead of an interface

--- src/server/adapter/adapter.go
+++ src/server/adapter/adapter.go
@@ -10,11 +10,11 @@ type Adapter interface {
 }
 
 // Make it easy to get the correct type out, no need for casting based on "HandleableType"
-type Handleable interface {
+type Handleable struct {
 
-	Type() HandleableType
-	Command() handler.Command
-	Query() handler.Query
+	typ HandleableType
+	command handler.Command
+	query handler.Query
 }
 
 type HandleableType string
@@ -24,37 +24,14 @@ const (
 	QRY = "query"
 )
 
-// Simple implementation
-type SimpleHandleable struct {
-
-	typ HandleableType
-	cmd handler.Command
-	qry handler.Query
-}
-
-func (h *SimpleHandleable) Type() {
-
-	return h.typ;
-}
-
-func (h *SimpleHandleable) Command() handler.Command {
-
-	return h.cmd
-}
-
-func (h *SimpleHandleable) Query() handler.Query {
-
-	return h.qry
-}
-
 // Helper methods to make creating it easier
-func NewCommand(cmd handler.Command) Handleable {
+func NewCommand(cmd handler.Command) *Handleable {
 
-	return &SimpleHandleable{CMD, cmd, nil}
+	return &Handleable{CMD, cmd, nil}
 }
 
-func NewQuery(qry handler.Query) Handleable {
+func NewQuery(qry handler.Query) *Handleable {
 
-	return &SimpleHandleable{QRY, qry, nil}
+	return &Handleable{QRY, qry, nil}
 }
 
