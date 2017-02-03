5bfdd5338e1ad15ffaf51c4de20236a5118085cd
Colin Lyons <colin@tercet.io>
2017-02-03 16:45:19 +0000

    Getting rid of sample code.

--- src/main/main.go
+++ src/main/main.go
@@ -9,18 +9,5 @@ import (
 
 func main() {
 
-	repository_projection := projection.CreateRepository()
-	repository_projection.Save(application.ListDatabasesProjection)
 
-	query_handler := query.CreateHandler(repository_projection)
-	query_handler.(query.SimpleHandler).Add(
-		application.ListDatabasesQueryID,
-		application.ListDatabasesQueryHandler,
-	)
-
-	result, _ := query_handler.Handle(application.ListDatabasesQuery)
-
-	result_string, _ := result.MarshalJSON()
-
-	fmt.Printf(string(result_string))
 }
